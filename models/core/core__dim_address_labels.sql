302f252b2c10b344bb91c48ae060d9f0e180eee7
Jack Forgash <jmfxyz@pm.me>
2022-12-09 16:37:24 -0700

    one labels view

--- models/core/core__dim_address_labels.sql
+++ models/core/core__dim_address_labels.sql
@@ -3,18 +3,70 @@
     secure = true
 ) }}
 
+WITH labels AS (
+
+    SELECT
+        blockchain,
+        address,
+        creator,
+        l1_label AS label_type,
+        l2_label AS label_subtype,
+        address_name AS label,
+        project_name
+    FROM
+        {{ source(
+            'labels',
+            'address_labels'
+        ) }}
+    WHERE
+        blockchain = 'terra'
+),
+tokens AS (
+    SELECT
+        blockchain,
+        'token_deployment' AS creator,
+        tx_id,
+        label,
+        symbol,
+        address,
+        decimals
+    FROM
+        {{ ref('silver__token_labels') }}
+),
+FINAL AS (
+    SELECT
+        COALESCE(
+            t.blockchain,
+            l.blockchain
+        ) AS blockchain,
+        COALESCE(
+            t.address,
+            l.address
+        ) AS address,
+        COALESCE(
+            t.creator,
+            l.creator
+        ) AS creator,
+        l.label_type,
+        l.label_subtype,
+        COALESCE(
+            t.symbol,
+            l.label
+        ) AS label,
+        COALESCE(
+            t.label,
+            l.project_name
+        ) AS project_name,
+        t.decimals,
+        t.tx_id AS deployment_tx_id
+    FROM
+        labels l full
+        JOIN tokens t USING (
+            blockchain,
+            address
+        )
+)
 SELECT
-    blockchain,
-    address,
-    creator,
-    l1_label AS label_type,
-    l2_label AS label_subtype,
-    address_name AS label,
-    project_name
+    *
 FROM
-    {{ source(
-        'labels',
-        'address_labels'
-    ) }}
-WHERE
-    blockchain = 'terra'
+    FINAL
---
+++ models/silver/silver__token_labels.sql
@@ -0,0 +1,39 @@
+{{ config(
+    materialized = "incremental",
+    cluster_by = ["_inserted_timestamp::DATE"],
+    unique_key = "contract_address",
+) }}
+
+WITH token_labels AS (
+
+    SELECT
+        block_timestamp,
+        tx_id,
+        message_value :msg :name :: STRING AS label,
+        message_value :msg :symbol :: STRING AS symbol,
+        IFF(
+            attributes :instantiate :_contract_address IS NOT NULL,
+            attributes :instantiate :_contract_address,
+            attributes :reply :_contract_address
+        ) :: STRING AS address,
+        message_value :msg :decimals :: INT AS decimals,
+        _ingested_at,
+        _inserted_timestamp
+    FROM
+        {{ ref("silver__messages") }}
+    WHERE
+        message_value :msg :decimals IS NOT NULL
+        AND {{ incremental_load_filter("_inserted_timestamp") }}
+)
+SELECT
+    'terra' as blockchain,
+    block_timestamp,
+    tx_id,
+    label,
+    symbol,
+    address,
+    decimals,
+    _ingested_at,
+    _inserted_timestamp
+FROM
+    token_labels
